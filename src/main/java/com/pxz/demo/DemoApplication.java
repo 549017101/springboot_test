eca7b8f40f64b2b47f8e06ee8cadaf565d803221
pxz <549017101@qq.com>
2021-01-06 17:36:15 +0800

    test

--- src/main/java/com/pxz/demo/DemoApplication.java
+++ src/main/java/com/pxz/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.pxz.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author pxz
+ */
 @SpringBootApplication
 public class DemoApplication {
 	
